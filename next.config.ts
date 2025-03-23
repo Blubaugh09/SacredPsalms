fec2e453be2792e75ddd0902a05ffaf6dd0fdad8
Blubaugh09 <blu.anesthesia@gmail.com>
2025-03-23 19:50:19 -0400

    Initial commit with complete project

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  compiler: {
-    styledComponents: true,
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-};
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
